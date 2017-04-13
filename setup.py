1a7b22ad15f2f2fad26365805ad1a54623f55343
Ajay Chinta <chinta512@gmail.com>
2017-04-13 12:05:12 +0530

    corrected path

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import sys
 import os
-import urllib
+from urllib.requests import urlretrieve
 import subprocess
 
 #install anaconda2 if not already installed
@@ -9,12 +9,11 @@ def install_anaconda():
 	anaconda_dir = homedir + '/anaconda3'
 	if not os.path.isdir(anaconda_dir):
 		anaconda_file = 'anaconda.sh'
-		urllib.urlretrieve('https://repo.continuum.io/archive/Anaconda2-4.3.1-Linux-x86_64.sh',anaconda_file)
+		urlretrieve('https://repo.continuum.io/archive/Anaconda2-4.3.1-Linux-x86_64.sh',os.path.join(homedir,anaconda_file))
 		subprocess.call("bash {0} -b -p {1}".format(os.path.join(homedir,anaconda_file),os.path.join(homedir,'anaconda2')).split())
-		subprocess.call('export PATH="{0}/anaconda2/bin:$PATH"'.format(homedir))
-
-		if install_result == 0:
-			subprocess.call()
+		cmd_set_path = 'export PATH="{0}/anaconda2/bin:$PATH"'.format(homedir)
+		print('setting path using : ', cmd_set_path)
+		subprocess.call(cmd_set_path.split())
 
 if __name__ == '__main__':
 	install_anaconda()
