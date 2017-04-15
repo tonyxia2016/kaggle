6de0d3cb559856197476a996708ec84bf53f6b83
Ubuntu <ubuntu@ip-172-31-21-252.ap-southeast-1.compute.internal>
2017-04-15 06:38:34 +0000

    adding utils

---
+++ dogs-vs-cats/setup.sh
@@ -0,0 +1,3 @@
+wget https://raw.githubusercontent.com/fastai/courses/master/deeplearning1/nbs/vgg16.py
+wget https://raw.githubusercontent.com/fastai/courses/master/deeplearning1/nbs/vgg16bn.py
+wget https://raw.githubusercontent.com/fastai/courses/master/deeplearning1/nbs/utils.py
