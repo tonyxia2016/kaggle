409c32b533cb1175e6eec7c9eccb46ac654dc452
Ajay Chinta <chinta512@gmail.com>
2017-04-15 15:38:36 +0000

    added gitignore

--- setup/install_conda_modules.sh
+++ setup/install_conda_modules.sh
@@ -1,4 +1,9 @@
 source /home/ubuntu/miniconda2/bin/activate
 conda install -y -q pandas
-conda install -y -q keras
+conda install -y -q keras=1.2.2
 conda install -y -q jupyter
+conda install -y -q matplotlib
+pip install image
+conda install -y -q scikit-learn
+conda install -y -q bcolz
+
